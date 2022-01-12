347cb52d10c9c4875f5f022924c46c2dc1f42bcc
shalini Kumarii <bittukumari@gmail.com>
2022-01-12 14:36:18 -0800

    First Jsx commit

---
+++ src/comopnents/FirstClassComponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+    
+    
+class FirstClassComponent extends Component {
+    render() {
+        return <div>This is our first class component.</div>;
+    }
+}
+    
+export default SomeClassComponent;
